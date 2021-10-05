32d8f581560351fdb98c96717f275dffd2d8d0c5
Vladimir Kolomeytsev <vsvetliy@gmail.com>
2021-10-06 01:07:31 +0700

    вапвыап

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const { celebrate, Joi } = require('celebrate');
-const usersRout = require('./routes/users');
-const usersControl = require('./controllers/users');
-const cardsRout = require('./routes/cards');
-const auth = require('./middlewares/auth');
-const errors = require('./middlewares/errors');
-const NotFoundError = require('./errors/not-found-err');
-const { requestLogger, errorLogger } = require('./middlewares/logger');
+// const express = require('express');
+// const mongoose = require('mongoose');
+// const { celebrate, Joi } = require('celebrate');
+// const usersRout = require('./routes/users');
+// const usersControl = require('./controllers/users');
+// const cardsRout = require('./routes/cards');
+// const auth = require('./middlewares/auth');
+// const errors = require('./middlewares/errors');
+// const NotFoundError = require('./errors/not-found-err');
+// const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const allowedCors = [
   'http://mesto.kolomeytsev.nomoredomains.club',
@@ -25,35 +25,6 @@ const app = express();
 app.use(express.json());
 // логгер запросов
 app.use(requestLogger);
-
-app.post('/signin', celebrate({
-  body: Joi.object().keys({
-    email: Joi.string().required().min(2).max(30),
-    password: Joi.string().required().min(2),
-  }),
-}), usersControl.usersLogin);
-
-app.post('/signup', celebrate({
-  body: Joi.object().keys({
-    name: Joi.string().min(2).max(30),
-    about: Joi.string().min(2).max(30),
-    avatar: Joi.string(),
-    email: Joi.string().required().min(2).max(30),
-    password: Joi.string().required().min(2),
-  }),
-}), usersControl.usersPost);
-
-app.use(auth);
-//app.use('/users', usersRout);
-
-//app.use('/cards', cardsRout);
-
-//app.use('/*', (req, res) => {
-  throw new NotFoundError('Cтраница не найдена');
-});
-//  логгер ошибок
-app.use(errorLogger);
-app.use(errors);
 
 app.use((req, res, next) => {
   const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
@@ -79,6 +50,35 @@ app.use((req, res, next) => {
 
   next();
 });
+
+app.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().min(2).max(30),
+    password: Joi.string().required().min(2),
+  }),
+}), usersControl.usersLogin);
+
+app.post('/signup', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string(),
+    email: Joi.string().required().min(2).max(30),
+    password: Joi.string().required().min(2),
+  }),
+}), usersControl.usersPost);
+
+app.use(auth);
+app.use('/users', usersRout);
+
+app.use('/cards', cardsRout);
+
+app.use('/*', (req, res) => {
+  throw new NotFoundError('Cтраница не найдена');
+});
+//  логгер ошибок
+app.use(errorLogger);
+app.use(errors);
 
 app.listen(PORT, () => {
   console.log(`App listening on porl ${PORT}`);
