e4723bbdfe4087058de3dcbea0bb3d9a8a018b08
Vladimir Kolomeytsev <vsvetliy@gmail.com>
2021-10-06 01:25:54 +0700

    asdas

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const { celebrate, Joi } = require('celebrate');
+const cors = require('cors');
 const usersRout = require('./routes/users');
 const usersControl = require('./controllers/users');
 const cardsRout = require('./routes/cards');
@@ -22,34 +23,35 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 });
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 // логгер запросов
 app.use(requestLogger);
 
-app.use((req, res, next) => {
-  const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
-  const { method } = req;
-  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-  // сохраняем список заголовков исходного запроса
-  const requestHeaders = req.headers['access-control-request-headers'];
-  console.log(123);
-  // проверяем, что источник запроса есть среди разрешённых
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', '*');
-  }
-  if (method === 'OPTIONS') {
-  // разрешаем кросс-доменные запросы любых типов (по умолчанию)
-    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-  }
-  if (method === 'OPTIONS') {
-  // разрешаем кросс-доменные запросы с этими заголовками
-    res.header('Access-Control-Allow-Headers', requestHeaders);
-    // завершаем обработку запроса и возвращаем результат клиенту
-    return res.end();
-  }
+// app.use((req, res, next) => {
+//   const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
+//   const { method } = req;
+//   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+//   // сохраняем список заголовков исходного запроса
+//   const requestHeaders = req.headers['access-control-request-headers'];
+//   console.log(123);
+//   // проверяем, что источник запроса есть среди разрешённых
+//   if (allowedCors.includes(origin)) {
+//     res.header('Access-Control-Allow-Origin', '*');
+//   }
+//   if (method === 'OPTIONS') {
+//   // разрешаем кросс-доменные запросы любых типов (по умолчанию)
+//     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+//   }
+//   if (method === 'OPTIONS') {
+//   // разрешаем кросс-доменные запросы с этими заголовками
+//     res.header('Access-Control-Allow-Headers', requestHeaders);
+//     // завершаем обработку запроса и возвращаем результат клиенту
+//     return res.end();
+//   }
 
-  next();
-});
+//   next();
+// });
 
 app.post('/signin', celebrate({
   body: Joi.object().keys({
