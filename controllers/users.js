6b5bc16f54d77904e54bca55834c14fd09fc780c
Vladimir Kolomeytsev <vsvetliy@gmail.com>
2021-10-06 05:02:14 +0700

    asdasd

--- controllers/users.js
+++ controllers/users.js
@@ -67,6 +67,7 @@ exports.usersLogin = function (req, res, next) {
           // token - наш JWT токен, который мы отправляем
           maxAge: 604800000,
           httpOnly: true,
+          domain: '.localhost',
         })
         .send({ message: 'Авторизация успешна' })
         .end();
